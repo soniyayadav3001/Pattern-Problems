5f6cb4b83691c590f085d70c0c39ad5ce2e8bde1
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-09 17:05:43 +0530

    Create Number_triangle.java

---
+++ Number_triangle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 8;
+        int i;
+        for (i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
+    }
+
+}
