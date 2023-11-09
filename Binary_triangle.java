1cd01b91e52fa010cebc16a4c6bac64436cce1ba
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-09 17:13:10 +0530

    Create Binary_triangle.java

---
+++ Binary_triangle.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 6;
+        int i;
+        int j;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
