ec5e9351a33e25414dbe0a1467bb352fabc7f5df
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-09 17:03:59 +0530

    Create Triangle_pattern2.java

---
+++ Triangle_pattern2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 8;
+        int i;
+        for (i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
