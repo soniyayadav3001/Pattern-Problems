577cee6923322dadb5e0c8526fd3dc81a1c4ad27
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-11-20 18:26:58 +0530

    Create Alphabet pyramid.java

---
+++ Alphabet pyramid.java
@@ -0,0 +1,19 @@
+public class Pattern{
+    public static void main(String[]args){
+        int n = 5;
+        int m = 5;
+        char ch ='A';
+        int space;
+        for(int i =1;i<=n;i++){
+            for(space=1;space<=n-i;space++){
+                System.out.print(" ");
+            }
+            for(int j =1;j<=i;j++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+
+}
